03a3d1032d6daed1f3ea02095eac188ca2de85af
vineet-singh-saini <singhsvineetrog@gmail.com>
2025-08-02 13:13:33 +0530

    App fix 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './Pages/Home/Home'
 import Login from './Pages/Login/Login'
 import Register from './Pages/Register/Register'
 import { BrowserRouter as Router, Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function App() {
   const user = JSON.parse(localStorage.getItem('user'));
